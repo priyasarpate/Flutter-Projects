8f6104e1a80927d6e58eef80ce5b3a5a644a4df5
Gunpriya Sarpate <sarpatepriya@gmail.com>
2025-09-18 13:57:53 +0530

    weather app

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -6,14 +6,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key});
-
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: weather_page(),
+      debugShowCheckedModeBanner: false,
+      // Fix: Corrected the name to match the class `WeatherPage`
+      home: WeatherPage(),
     );
+  }
 }
-} 
---
+++ weather_app/lib/model/weather_model.dart
@@ -0,0 +1,12 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String mainCondition;
+
+  Weather ({
+    required this.cityName,
+    required this.temperature,
+    required this.mainCondition,
+  });
+}
+
