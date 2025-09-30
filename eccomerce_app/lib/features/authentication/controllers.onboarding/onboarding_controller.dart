c0745ce86c8cdf04ac80703cac4c400d7197fda2
Gunpriya Sarpate <sarpatepriya@gmail.com>
2025-09-30 23:36:51 +0530

    working on onboarding

---
+++ eccomerce_app/lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+class OnboardingController extends GetxController{
+static OnboardingController get instance => Get.find();
+
+//variables
+
+//update current index when page scroll
+
+//jump to the specific dot selected page
+
+//update current index and jump to the next page 
+
+//update current index and jump to the last page 
+}
