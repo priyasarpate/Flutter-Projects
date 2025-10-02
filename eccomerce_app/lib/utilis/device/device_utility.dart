6f86bc14d113b46ec0d9a798e9a9b50d47a74663
Gunpriya Sarpate <sarpatepriya@gmail.com>
2025-10-02 09:45:14 +0530

    formatter controller

--- eccomerce_app/lib/utilis/device/device_utility.dart
+++ eccomerce_app/lib/utilis/device/device_utility.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class TDeviceUtils {
