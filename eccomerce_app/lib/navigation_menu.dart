7d17f901678ed0eee254fc0e377dfbc10f9b487b
Gunpriya Sarpate <sarpatepriya@gmail.com>
2025-12-01 11:04:38 +0530

    navigation selector

--- eccomerce_app/lib/navigation_menu.dart
+++ eccomerce_app/lib/navigation_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class NavigationMenu extends StatelessWidget {
@@ -6,13 +7,32 @@ class NavigationMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+ final controller = Get.put(NavigationController());
+
     return Scaffold(
-    bottomNavigationBar: NavigationBar(destinations: [
-     const NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-     const NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
-     const NavigationDestination(icon: Icon(Iconsax.heart), label: 'Whishlist'),
-     const NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
-    ]),
+    bottomNavigationBar: Obx(
+      () => NavigationBar(
+        height: 80,
+        elevation: 0,
+        selectedIndex: controller.selectedIndex.value,
+        //function 
+        onDestinationSelected: (index) => controller.selectedIndex.value = index,
+      destinations: [
+       const NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+       const NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+       const NavigationDestination(icon: Icon(Iconsax.heart), label: 'Whishlist'),
+       const NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+      ]),
+    ),
+    body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
 }
+
+  //another stateful class for index selection
+  class NavigationController extends GetxController {
+    final Rx<int> selectedIndex = 0.obs;
+     
+    final screens = [Container(color: Colors.amber), Container(color: Colors.blue), Container(color: Colors.green), Container(color: Colors.amber),];
+  }
